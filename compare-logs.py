acb3b3ef1826b088ea876947d69d325f9d1c4c68
David Malcolm <dmalcolm@redhat.com>
2015-11-10 09:38:35 -0500

    compare-logs.py: use median rather than minimum

--- compare-logs.py
+++ compare-logs.py
@@ -3,10 +3,23 @@ import re
 
 from tabulate import tabulate
 
+def median(data):
+    if 0:
+        print(data)
+    data = sorted(data)
+    n = len(data)
+    if n == 0:
+        raise ValueError('no data')
+    elif n % 2 == 0:
+        return data[n // 2]
+    else:
+        raise ValueError('TODO')
+
 class BenchmarkLog:
     def __init__(self, title, path):
         self.title = title
         r = OrderedDict()
+        stat = None
         for line in open(path):
             if 0:
                 print(repr(line))
@@ -14,19 +27,34 @@ class BenchmarkLog:
             m = re.match('(compare_.+): (.+)', line)
             if m:
                 #print('MATCH: %r' % (m.groups(), ))
+                #print('stat: %r' % (stat, ))
+                if stat and data['control'] and data['experiment']:
+                    old_median = median(data['control'])
+                    new_median = median(data['experiment'])
+                    if 0:
+                        print('Median: %s -> %s' % (old_median, new_median))
+                    r[stat] = (old_median, new_median)
+                    #print(data)
+                data = {'control':[], 'experiment':[]}
                 stat = m.groups()
                 if stat[1].startswith("xgcc '"):
                     stat = stat[0], 'xgcc %s' % stat[1][6:-1]
+
             m = re.match('Min: (.+) -> (.+): .*', line)
             if m:
                 #print('MATCH: %r' % (m.groups(), ))
                 min_ = m.groups()
-                r[stat] = min_
-            #m = re.match('Avg: (.+)', line)
-            #if m:
-            #    #print('MATCH: %r' % (m.groups(), ))
-            #    avg = m.groups()
-            #    r[stat] = avg
+                # FIXME:
+                #r[stat] = min_
+            m = re.match('  iteration [0-9]+: (.+): (.+): time_taken: (.+)', line)
+            if m:
+                if 0:
+                    print('MATCH: %r' % (m.groups(), ))
+                peer, test, time_taken = m.groups()
+                time_taken = float(time_taken)
+                #print(time_taken)
+                data[peer].append(time_taken)
+
             m = re.match('Mem max: (.+) -> (.+): .*', line)
             if m:
                 r[stat] = m.groups()
@@ -70,7 +98,7 @@ logs = read_logs()
 titles = [log.title for log in logs]
 headers=['', 'Control'] + titles
 
-print("Minimal wallclock time (s) over 10 iterations")
+print("Median wallclock time (s) over 10 iterations")
 print("  (each experiment's % change is relative to 10 iterations of control interleaved with that experiment)")
 data = []
 for k, v in logs[0].iter_wallclock_items():
@@ -81,8 +109,8 @@ for k, v in logs[0].iter_wallclock_items():
         result = log.get_result(k)
         # We have a (control, experiment) pair of stringified floats;
         # get experiment:
-        local_control = float(result[0])
-        experiment = float(result[1])
+        local_control = round(float(result[0]), 5)
+        experiment = round(float(result[1]), 5)
         line.append('%s (%s)'
                     % (experiment,
                        percent_change(experiment, local_control)))
